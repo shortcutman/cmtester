61b57aecb96be6a5b64a3072e2d72f2e55ec9e16
Daniel Burke <danieljburke@me.com>
2016-07-24 21:15:14 +1000

    Initial layout

--- cmtester/FirstViewController.swift
+++ cmtester/FirstViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class LiveViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
--- cmtester/SecondViewController.swift
+++ cmtester/SecondViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class Historic: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
